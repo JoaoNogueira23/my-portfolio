5ef5c767387cebb1fad3a334acf9ba38ae6b0b3f
JoaoNogueira23 <joao.n.m23@gmail.com>
2024-09-03 18:53:46 -0300

    feat: new download button

--- src/components/pages/about/AboutPage.tsx
+++ src/components/pages/about/AboutPage.tsx
@@ -7,59 +7,56 @@ import "./about.scss"
 
 export default function AboutPage() {
     return(
-        <>
-            <div className="container-about" id='about'>
-                <h2 className="title-page">About</h2>
+        <div className="container-about" id='about'>
+            <h2 className="title-page">About</h2>
 
-                <div className="content-about">
-                
+            <div className="content-about">
+            
 
-                    <div className="profile">
-                        <Image 
-                        className="img-profile"
-                        width={300}
-                        height={290}
-                        alt="Profile Picture"
-                        src={"/perfil.JPEG"}
-                        />
+                <div className="profile">
+                    <Image 
+                    className="img-profile"
+                    width={300}
+                    height={290}
+                    alt="Profile Picture"
+                    src={"/perfil.JPEG"}
+                    />
 
-                        <p>João Martins</p>
+                    <p>João Martins</p>
 
-                        <div className="social-icons">
-                            <a href="mailto:jvictor.n.m23@gmail.com" target="_blank">
-                                <IconEmail/>
-                            </a>
+                    <div className="social-icons">
+                        <a href="mailto:jvictor.n.m23@gmail.com" target="_blank">
+                            <IconEmail/>
+                        </a>
 
-                            <a href="https://www.linkedin.com/in/jvnogueira23/" target="_blank">
-                                <LinkedInIcon />
-                            </a>
+                        <a href="https://www.linkedin.com/in/jvnogueira23/" target="_blank">
+                            <LinkedInIcon />
+                        </a>
 
-                            <a href="https://github.com/JoaoNogueira23" target="_blank">
-                                <GitHubIcon />
-                            </a>
-                            
-                            
-                            
-                        </div>
-                    </div>
-
-                    <div className="profile-description">
+                        <a href="https://github.com/JoaoNogueira23" target="_blank">
+                            <GitHubIcon />
+                        </a>
                         
-                        <p>
-                        Eu sou um deseenvolvedor Full Stack com conhecimento sólido com <span className="text-emphasis">React, Python, AWS e Banco de Dados Relacionais</span> e familiaridade
-                        com tecnologias como <span className="text-emphasis">AWS (Cloud Computer), Azure</span>, e familiaridade com ferramentas de <span className="text-emphasis">LOW CODE como PowerBI e Power Automate</span>, além de ferramentas
-                        de versionamento como o GIT.
-                        </p>
-                        <br/>
-                        <p>
-                        Tenho um experiência profissional no mercado financeiro, onde somei ao time de desenvolvimento de automações
-                        no banco BTG Pactual por cerca de 2 anos, com destaca de web apps que desenvolvi gerando eficiência operacional e
-                        mitigando, também, risco operacional. Um dos apps <span className="text-emphasis">gerou um head count por mês de eficiência (168h)</span> .
-                        </p>
+                        
+                        
                     </div>
                 </div>
+
+                <div className="profile-description">
+                    
+                    <p>
+                    Eu sou um deseenvolvedor Full Stack com conhecimento sólido com <span className="text-emphasis">React, Python, AWS e Banco de Dados Relacionais</span> e familiaridade
+                    com tecnologias como <span className="text-emphasis">AWS (Cloud Computer), Azure</span>, e familiaridade com ferramentas de <span className="text-emphasis">LOW CODE como PowerBI e Power Automate</span>, além de ferramentas
+                    de versionamento como o GIT.
+                    </p>
+                    <br/>
+                    <p>
+                    Tenho um experiência profissional no mercado financeiro, onde somei ao time de desenvolvimento de automações
+                    no banco BTG Pactual por cerca de 2 anos, com destaca de web apps que desenvolvi gerando eficiência operacional e
+                    mitigando, também, risco operacional. Um dos apps <span className="text-emphasis">gerou um head count por mês de eficiência (168h)</span> .
+                    </p>
+                </div>
             </div>
-        </>
-        
+        </div>
     )
 }
--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
 import './homepage.scss'
 
 
 export default function HomePage(){
+
     return(
         <div className='container-home' id='home'>
             {/* div decribe */}
@@ -17,19 +17,16 @@ export default function HomePage(){
                     <p className="apresentation">
                     Eu me chamo João Victor, um desenvolvedor com especialidade no desenvolvimento de web apps e automações de rotinas operacionais.
                     </p>
-
-                    <a className="button-contact" href="mailto:jvictor.n.m23@gmail.com" target="_blank">
-                        Contate-me
-                    </a>
+                    <div className="links">
+                        <a className="button-contact" href="mailto:jvictor.n.m23@gmail.com" target="_blank">
+                            Contate-me
+                        </a>
+                        <a download href='./files/cv_pt.pdf' className='button-contact'>
+                            Download CV
+                        </a>
+                    </div>
+                    
                 </div>
-                
-                <Image
-                    src="/image_home.png"
-                    width={600}
-                    height={590}
-                    alt="Picture Home"
-                    className='image-home'
-                />
         </div>
     )
 }
