489125dc2791f156d01a662fd98d01967ac5fbfd
JoaoNogueira23 <jvictor.n.m23@gmail.com>
2024-10-10 20:58:29 -0300

    styles: better user experience with transition

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -15,7 +15,8 @@ export default function HomePage(){
                     Eleve o seu negócio com um desenvolvimento de qualidade!
                     </h2>
                     <p className="apresentation">
-                    Eu me chamo João Victor, um desenvolvedor com especialidade no desenvolvimento de web apps e automações de rotinas operacionais.
+                    Eu me chamo João Victor,  um desenvolvedor Full Stack com experiência no desenvolvimento de sistemas que melhorem rotinas
+                    operacionais e agregam valor ao negócio.
                     </p>
                     <div className="links">
                         <a className="button-contact" href="mailto:jvictor.n.m23@gmail.com" target="_blank">
